64906a9e6ba43ad121b9d8222e002965a65471c8
Alexey Badenkov <alexey.badenkov@gmail.com>
2013-06-04 17:29:11 +0400

    Merge

--- lib/wild_wild_hash.rb
+++ lib/wild_wild_hash.rb
@@ -3,4 +3,11 @@ require "wild_wild_hash/version"
 module WildWildHash
   autoload :Mash, 'wild_wild_hash/mash'
   autoload :Dash, 'wild_wild_hash/dash'
+<<<<<<< HEAD
+=======
+
+  module Extensions
+    autoload :MethodAccess, 'wild_wild_hash/extensions/method_access'
+  end
+>>>>>>> 162bb24... Merge
 end

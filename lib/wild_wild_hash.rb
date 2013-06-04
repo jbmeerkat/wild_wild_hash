5ea2ac00e90558dda36b5538650723b030af16bb
Alex Klyanchin <jetblackmeerkat@gmail.com>
2013-06-04 17:27:32 +0400

    methods from mash to extensions module

--- lib/wild_wild_hash.rb
+++ lib/wild_wild_hash.rb
@@ -2,4 +2,9 @@ require "wild_wild_hash/version"
 
 module WildWildHash
   autoload :Mash, 'wild_wild_hash/mash'
+
+  module Extensions
+    autoload :MethodAccess, 'wild_wild_hash/extensions/method_access'
+  end
+
 end
